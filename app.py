a60769fb4a3b00ca1b24ea4cde1b90b4cb96cafe
MNMTanmay <tanmay.saxena@marketsandmarkets.com>
2026-03-05 15:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ Query:
 """
 
     response = client.chat.completions.create(
-        model="mixtral-8x7b-32768",
+        model="llama-3.3-70b-versatile",
         messages=[{"role":"user","content":prompt}]
     )
 
@@ -177,7 +177,7 @@ Query:
 """
 
     response=client.chat.completions.create(
-        model="mixtral-8x7b-32768",
+        model="llama-3.3-70b-versatile",
         messages=[{"role":"user","content":prompt}]
     )
 
